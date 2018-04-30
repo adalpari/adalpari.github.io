7790a53621c4c46f603bd655de72e689337bcf2d
Adalberto <>
2018-04-30 21:20:23 +0200

    SRP post

--- _site/assets/js/lunr/lunr-store.js
+++ _site/assets/js/lunr/lunr-store.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"One of the multiple must that EVERY developer should do, is read: Clean Code book from Robert C. Martin. In that book, you will find basic and advanced rules and tips, to create good quality code in terms of readability. Easier to understand and mantain than a messy ugly code....","categories": [],
         "tags": [],
         "url": "http://localhost:4000/Clean-Code/",
-        "teaser":"http://localhost:4000/assets/images/code_snap.jpg"}]
+        "teaser":"http://localhost:4000/assets/images/code_snap.jpg"},{
+        "title": "S.O.L.I.D - Single responsability principle",
+        "excerpt":"The Single Responsability Principle (SRP), is the first of the 5 principles of a SOLID base.The name of this principle is quite explanatory: one object should have one, and only one resposanility. This is: have only one purpose. Why this principle is good for the code base?. It will contribute...","categories": [],
+        "tags": [],
+        "url": "http://localhost:4000/SOLID-S/",
+        "teaser":"http://localhost:4000/assets/images/bus.jpg"}]
